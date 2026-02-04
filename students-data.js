99198239bc1837dcb44354bc48e7d056115393d8
GAREANE A. I. ALGREANE <gareane@gmail.com>
2026-02-04 16:35:08 +0200

    Add files via upload

---
+++ students-data.js
@@ -0,0 +1,12 @@
+// Student Data File - Auto-generated
+// Last updated: [DATE_PLACEHOLDER]
+
+const studentsData = [
+    // سيتم ملء هذا الملف ببياناتك تلقائياً
+    // عند تشغيل سكريبت التحويل
+];
+
+// تصدير البيانات ليستخدمها student_search.html
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = studentsData;
+}
